2f8302b24d4b44e1c4686fcbc254e16dc31b69e0
mare16o <wareo16o@gmail.com>
2022-09-27 11:01:09 -0500

    herencia.act1

---
+++ Herencia/src/com/Clase_C.java
@@ -0,0 +1,29 @@
+package com;
+
+public class Clase_C extends Clase_A{
+	private String material;
+	
+	public Clase_C() {
+		
+	}
+
+	public Clase_C(String nombre, String apellido, int edad) {
+		super(nombre, apellido, edad);
+
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	
+	
+	
+	
+	
+	
+}
