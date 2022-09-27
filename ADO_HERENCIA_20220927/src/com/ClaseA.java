fe690c7802ad2da376606f51b66feaa306eb9e9b
ElAbriii <abrahamdoranates93@gmail.com>
2022-09-27 10:40:49 -0500

    Agregué mi herencia

---
+++ ADO_HERENCIA_20220927/src/com/ClaseA.java
@@ -0,0 +1,45 @@
+package com;
+
+public class ClaseA {
+
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	public ClaseA() {
+		
+	}
+
+	public ClaseA(String nombre, String apellido, int edad) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
+}
