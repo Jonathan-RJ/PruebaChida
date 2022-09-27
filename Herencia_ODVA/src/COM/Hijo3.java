b660ba6989deb92d84e14d2ddced6aba1fb34615
daveone76 <odva7694@gmail.com>
2022-09-27 11:02:00 -0500

    Agregando carpeta de herencia Onesimo David

---
+++ Herencia_ODVA/src/COM/Hijo3.java
@@ -0,0 +1,17 @@
+package COM;
+
+public class Hijo3 extends Padre{
+
+	
+	public Hijo3() {
+		
+	}
+
+	public Hijo3(String nombre, String apellido, int edad) {
+		super(nombre, apellido, edad);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
